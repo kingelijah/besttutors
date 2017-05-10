d950da4f6d26c14a7fa42bdcb013965cf4d7786d
kingelijah <ealeakhue@gmail.com>
2017-05-10 07:54:26 +0100

    add files

---
+++ best/besttutorhome.php
@@ -0,0 +1,97 @@
+<?php
+ require("besttutorindex.php");
+ ?>
+ 
+ <section class="firstpage"><!--first page-->
+ <div class="container trans">
+ <h1>HELLO,Welcome</h1>
+ <h2>A place where you can get "Professional Full-time tutors,inspirational mentors,expert education advice,and holiday schools."</h2>
+ <a href="getatutor.php"><button class="btn"><b>Get A Tutor</b></button></a>
+ <a href="besttutoraboutus.php"><button class="btn"><b>Learn more</b></button></a>
+ </div>
+ </section><!--end of first page-->
+ </br>
+ <div class="container description"><!--start of descrition page-->
+ <div class="col-md-10 col-md-offset-1">
+ <h3>Why Choose Best Tutors</h3>
+ <h4></b> ...Best tutor is one of the nation's fastest growing and most popular academic tutoring directory.</br>All of our tutors conduct their one-on-one tutoring sessions
+ in the comfort of the student's homes.</br>Our tutors come to you, if you are a student who is struggling with your studies, or the parent of a student who is having difficultieswith his or her 
+ coursework,then you've come to the right place.</h4>
+ </div>
+ </div><!--end of description page-->
+ <!--start of mdlcard-->
+ <div class="container">
+<div class="row">
+	<div class="col-md-4 col-sm-4">
+<div class="demo-card-square mdl-card mdl-shadow--2dp">
+  <div class="mdl-card__title1 mdl-card--expand">
+    </div>
+  <div class="mdl-card__supporting-text">
+  <h2 class="mdl-card__title-text"><b>Tutoring/Guidance</b></h2>
+   <h5><b>from homework help, intensive remediation or something in between.we also help you through...</b></h5>
+  </div>
+  <div class="mdl-card__actions mdl-card--border">
+    <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">
+      <a href="besttutorservices.php#tg"><b>Read more</b></a>
+    </a>
+  </div>
+</div>
+	</div>
+	<div class="col-md-4 col-sm-4">
+<div class="demo-card-square mdl-card mdl-shadow--2dp">
+  <div class="mdl-card__title2 mdl-card--expand">
+    
+  </div>
+  <div class="mdl-card__supporting-text">
+  <h2 class="mdl-card__title-text"><b>Adult Education</b></h2>
+    <h5><b>Education they say has no age limit, we at best tutor we have been in the business of bringing education...</b></h5>
+  </div>
+  <div class="mdl-card__actions mdl-card--border">
+    <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">
+      <a href="besttutorservices.php#ae"><b>Read more</b></a>
+    </a>
+  </div>
+</div>
+</div>
+	<div class="col-md-4 col-sm-4">
+<div class="demo-card-square mdl-card mdl-shadow--2dp">
+  <div class="mdl-card__title3 mdl-card--expand">
+    
+  </div>
+  <div class="mdl-card__supporting-text">
+  <h2 class="mdl-card__title-text"><b>Computer Training</b></h2>
+   <h5><b> Giving your child intensive and practical computer training is no longer optional...</b></h5>
+  </div>
+  <div class="mdl-card__actions mdl-card--border">
+    <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">
+     <a href="besttutorservices.php#tg"><b>Read more</b></a>
+    </a>
+  </div>
+</div>
+	</div>
+	</div>
+	</div>
+	<!--end of mdlcard-->
+<br/><br/>
+<div class="container"><!--vetting container-->
+<div class="row vetting">
+<div class="col-md-4 col-sm-4">
+<div class="ribbon2"><i class="fa fa-user-o fa-5x"></i></div>
+<p>our tutors are professional teachers,masters in their fields well trained and fully vetted by us.</p>
+</div>
+<div class="col-md-4 col-sm-4">
+<div class="ribbon3"><i class="fa fa-check-square-o fa-5x"></i></div>
+<p>we monitor your child's progress and make sure you attain your goals of the program.</p>
+</div>
+<div class="col-md-4 col-sm-4">
+<div class="ribbon4"><i class="fa fa-pencil-square-o fa-5x"></i></div>
+<p>All you have to do is get a tutor and we will handle the rest,from finding and matching you with the perfect tutor that fits your special needs to logistics.</p>
+</div>
+</div>
+</div><!--end of vetting container-->
+<footer><!--start footer-->
+<div class="container">
+<p>web powered by emperor technologies</br>ealeakhue@gmail.com</p>
+<p>find us on social media</p>
+</div>
+</footer><!--end of footer-->
