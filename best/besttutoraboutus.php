e6b946bce6813c31afb28fa6a715beb193fe1ad8
kingelijah <ealeakhue@gmail.com>
2017-05-10 08:00:02 +0100

    add files

---
+++ best/besttutoraboutus.php
@@ -0,0 +1,25 @@
+<?php
+ require("besttutorindex.php");
+ ?>
+ <div class="container about">
+ <h2 >Best tutors</h2>
+ <h4 class="abouth4">...Best tutor is one of the nation's fastest growing and most popular academic tutoring directory.</br>All of our tutors conduct their one-on-one 
+ tutoring sessions
+ in the comfort of the student's homes.</br>Our tutors come to you, if you are a student who is struggling with your studies, or the parent of a 
+ student who is having difficulties with his or her 
+ coursework,then you've come to the right place.</br>We offer tutoring for all ages in all academic subjects.</br>Hiring a friendly,experienced and approachable 
+ tutor is an excellent way to provide 
+ to provide the support and confidence that the student needs to reach his or her full potential in school.</br>Best tutor believes that a highly 
+ individualized,one-on-one 
+ tutoring relationship is the best method for rapidly improving a student's academic performance.</br> Our tutors have worked individually with students 
+ from a variety of skill levels
+ and age groups,including primary,secondary school and college.</br>They are experts in helping students to improve their grades,raise their test scores,
+ build effective study skills 
+ and develop a sense of academic self confidence.</br>We listen to the child because we understand that each child has a specific learning style,we also find what 
+ motivates and interests the child just to ensure we give your child that passion and also the confidence to perform excellently well.</br>In extreme 
+ cases we go as far as calling your child's school
+ teacher to enquire about their progress,scores on standardized and notes on possible personality problems.</br>Don't let your child falls way behind 
+ the class and never recovers 
+recovers.</br>The sooner you find a tutor the sooner your child will be on top of his or her studies.</br>So don't delay and find a tutor today. 
+</h4>
+ </div>
