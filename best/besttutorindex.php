0294d0d4803d22920ba9b400c9838376eb68e218
kingelijah <ealeakhue@gmail.com>
2017-05-10 07:58:49 +0100

    add files

---
+++ best/besttutorindex.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+     <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Pirata One">
+	<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Bilbo">
+	<link rel="stylesheet" href="mdl/material.min.css">
+    <script src="mdl/material.min.js"></script>
+   <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+   <link rel="stylesheet" href="font/css/font-awesome.min.css">
+   <link rel="stylesheet"  type="text/css" href="beststyless.css">
+   <title>Best Tutors </title>
+</head>
+<body>
+<br/>
+<!--start navbar-->
+<nav id="mynavbar" class="navbar-inverse navbar-fixed-top" role="navigation">
+                    <div class="container">
+                        <div class="navbar-header">
+                            <button type="button"class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                                <span class="sr-only">toggle navigation</span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                            </button>
+                        </div>
+                        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                            <ul class="nav navbar-nav" >
+                                <li><div class="dropdown"><a href="besttutorhome.php">Home</a></div></li>
+                                <li><div class="dropdown"><a href="besttutoraboutus.php">About Us</a></div></li>
+								<li><div class="dropdown"><a href="besttutorcontactus.php">Contact Us</a></div></li>
+                       <li><div class="dropdown">
+                    <a href="besttutorservices.php" class="dropbtn">Our Services<b class="caret"></b></a>
+                    <div class="dropdown-content">
+                        <a href="besttutorservices.php#tg">Tutoring/career guidance</a>
+                        <a href="besttutorservices.php#ae">Adult Education</a>
+                        <a href="besttutorservices.php#ct">Computer Training</a>
+                    </div></div></li>
+								<li><div class="dropdown"><a href="getatutor.php">Get A Tutor</a></div></li>
+								<li><div class="dropdown">
+                        <a href="#" class="dropbtn">Exclusive Tips<b class="caret"></b></a>
+                        <div class="dropdown-content">
+                        <a href="examtips.php">Exam tips</a>
+                        <a href="careerguide.php">career guides</a>
+                        </div></div></li>
+                                    </ul>
+                                </li>
+								</ul>
+                        </div>
+						</div>
+			            </div>
+</nav>
+<!--end navbar-->
+</br>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <script src="bootstrap/js/bootstrap.min.js"></script>
+		</body>
+		</html>
