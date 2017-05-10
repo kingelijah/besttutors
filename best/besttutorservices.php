1674a1e4da2ffeb5312c3ce25d9a1a8790dec08b
kingelijah <ealeakhue@gmail.com>
2017-05-10 08:00:47 +0100

    add files

---
+++ best/besttutorservices.php
@@ -0,0 +1,25 @@
+<?php
+ require("besttutorindex.php");
+ ?>
+ <div class="container">
+<div class="col-md-10 col-md-offset-1 service">
+<div id="tg">
+<h2>Tutoring/Guidance</h2>
+<h3>from homework help,intensive remediation or something in between.we also help you through that rigorous process of preparation and practice for that 
+particular exam to ensure good success.
+</h3>
+
+</div>
+
+<div id="ae">
+<h2>Adult Education</h2>
+<h3>Education they say has no age limit, we at best tutor we have been in the business of bringing education to children,youths and adults.It is very possible
+to reach that height of academic excellence you have always dreamed about,with our well tailored time table just to suit your needs .We are very confident we'll give you the best.</h3>
+</div>
+<div id="ct">
+<h2>Computer Training</h2>
+<h3>Giving your child intensive and practical computer training is no longer optional.The importance of computer education cannot be overemphasized,that's why at Best tutor 
+we have decided to bring together computer experts to give your child that edge,from elementary computer training to advance knowledge up to computer coding.</h3>
+</div>
+</div>
+</div>
